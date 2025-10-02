ac123b6343d32edf418fa8b039970a248c192187
Slanted Vision <git@slantedvision.com>
2025-10-02 10:46:42 -0400

    Added A10 Sound File

--- FuMedia.lua
+++ FuMedia.lua
@@ -42,6 +42,7 @@ LSM:Register("sound", "fireeverything", [[Interface\Addons\SharedMedia_FuMedia\s
 LSM:Register("sound", "zelda_chest_opening", [[Interface\Addons\SharedMedia_FuMedia\sound\zelda_chest_opening.ogg]]) 
 LSM:Register("sound", "zelda_chest_tada", [[Interface\Addons\SharedMedia_FuMedia\sound\zelda_chest_tada.ogg]]) 
 LSM:Register("sound", "FFT_Complete", [[Interface\Addons\SharedMedia_FuMedia\sound\complete1.ogg]]) 
+LSM:Register("sound", "A10-Guns", [[Interface\Addons\SharedMedia_FuMedia\sound\A10Warthog.ogg]]) 
 
 -- -----
 --   STATUSBAR
